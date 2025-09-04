02a3abe0a15b3fb37feb6495427b5883b2f0508a
Azaz Arfin <azazarfin54@gmail.com>
2025-09-05 03:11:00 +0600

    Port fix

--- src/server.js
+++ src/server.js
@@ -54,4 +54,6 @@ app.use('/api/chapters', chapterRoutes);
 
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
